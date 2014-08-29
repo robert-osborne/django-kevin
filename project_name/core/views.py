4b4dbffc2ac5640795f8d0585cd8d7341fe27693
Kevin Xu <kevin@imkevinxu.com>
2014-08-28 19:47:11 -0700

    renamed base app to core app

--- project_name/core/views.py
+++ project_name/core/views.py
@@ -1,10 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render, redirect
+
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 from django.contrib import messages
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-index = IndexView.as_view()
+index = cache_page(60 * 10)(IndexView.as_view())
