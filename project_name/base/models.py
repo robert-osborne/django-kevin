89753687a6f0df9887a7be2a63d95a95485edd3e
Kevin Xu <kevin@imkevinxu.com>
2014-08-28 19:46:12 -0700

    added common model imports

--- project_name/base/models.py
+++ project_name/base/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils.functional import cached_property
+
+# Common Superclasses
+from model_utils.models import TimeStampedModel
+from authtools.models import AbstractNamedUser
 
 # Create your models here.
