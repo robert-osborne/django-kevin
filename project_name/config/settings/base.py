9b036800be257c9a0fed87d15d984c79f7786315
Kevin Xu <kevin@imkevinxu.com>
2014-09-01 19:52:45 -0700

    added Procfile.dev to interpret and rid of htmlmin

--- project_name/config/settings/base.py
+++ project_name/config/settings/base.py
@@ -198,7 +198,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'htmlmin.middleware.MarkRequestMiddleware',
 )
 ########## END MIDDLEWARE CONFIGURATION
 
