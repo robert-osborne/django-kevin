4ebf9b53857ae462b2867fcc5a5101a55ed3ec1f
Kevin Xu <kevin@imkevinxu.com>
2014-09-03 19:57:40 -0700

    added favicon.ico and robots.txt

--- project_name/config/urls.py
+++ project_name/config/urls.py
@@ -2,9 +2,15 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.contrib import admin
 
+from django.views.generic.base import RedirectView, TemplateView
+
 urlpatterns = patterns('',
-    # Base URLs
+    # Core URLs
     url(r'^', include('core.urls', namespace='core')),
+
+    # Root-level redirects for common browser requests
+    url(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'img/favicon.ico')),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
 
     # Authtools URLs
     # https://github.com/fusionbox/django-authtools/blob/master/authtools/urls.py
@@ -16,8 +22,8 @@ urlpatterns = patterns('',
 )
 
 # Uncomment the next line to serve media files in dev.
-# from django.conf.urls.static import static
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.conf.urls.static import static
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
