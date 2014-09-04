fd5194d7f23b65f9ffb74edb7fbbd4f9a3e4eb8a
Kevin Xu <kevin@imkevinxu.com>
2014-09-03 18:57:30 -0700

    updated to 1.7 app configuration

--- project_name/config/urls.py
+++ project_name/config/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
-
 from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = patterns('',
     # Base URLs
--- project_name/core/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.utils.functional import cached_property
-
-# Common Superclasses
-from model_utils.models import TimeStampedModel
-from authtools.models import AbstractNamedUser
-
-# Create your models here.
