e35aeb4c9e0a70e42d08b4cb0e06b54393c4eb49
Kevin Xu <kevin@imkevinxu.com>
2014-09-03 18:58:20 -0700

    added send_mail and 'get_current_site' shortcuts in views.py

--- project_name/core/views.py
+++ project_name/core/views.py
@@ -2,9 +2,11 @@ from django.views.generic import TemplateView, RedirectView
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render, redirect
 
+from django.contrib.sites.shortcuts import get_current_site
 from django.views.decorators.cache import cache_page
 from django.core.cache import cache
 from django.contrib import messages
+from django.core.mail import send_mail
 
 
 class IndexView(TemplateView):
