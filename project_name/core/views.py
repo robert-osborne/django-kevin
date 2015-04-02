e22167de2fb36de1a613f5f8e3571615552a1022
Kevin Xu <kevin@imkevinxu.com>
2015-04-02 16:44:09 -0700

    Upgrading cache time

--- project_name/core/views.py
+++ project_name/core/views.py
@@ -5,4 +5,4 @@ from django.views.decorators.cache import cache_page
 class HomeView(TemplateView):
     template_name = 'pages/home.html'
 
-home = cache_page(60 * 10)(HomeView.as_view())
+home = cache_page(60 * 60 * 24)(HomeView.as_view())
