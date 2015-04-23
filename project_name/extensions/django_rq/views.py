4ebe2283bcfeda383c7dd4351beb8a724cc695bc
Kevin Xu <kevin@imkevinxu.com>
2015-04-23 12:36:39 -0700

    Added started, deferred, and finished jobs stats to django_rq extensions

--- project_name/extensions/django_rq/views.py
+++ project_name/extensions/django_rq/views.py
@@ -15,6 +15,7 @@ from redis.exceptions import ResponseError
 from rq import requeue_job, Worker
 from rq.job import Job
 from rq.exceptions import NoSuchJobError, InvalidJobOperationError
+from rq.registry import DeferredJobRegistry, FinishedJobRegistry, StartedJobRegistry
 
 from django_rq.queues import get_connection, get_queue_by_index
 from django_rq.settings import QUEUES_LIST
@@ -80,18 +81,34 @@ def queues(request):
     queues = []
     for index, config in enumerate(QUEUES_LIST):
         queue = get_queue_by_index(index)
+        connection = queue.connection
+
         queue_data = {
             'name': queue.name,
             'jobs': queue.count,
             'index': index,
+            'connection_kwargs': connection.connection_pool.connection_kwargs
         }
+
         if queue.name == 'failed':
             queue_data['workers'] = '-'
+            queue_data['finished_jobs'] = '-'
+            queue_data['started_jobs'] = '-'
+            queue_data['deferred_jobs'] = '-'
+
         else:
             connection = get_connection(queue.name)
             all_workers = Worker.all(connection=connection)
             queue_workers = [worker for worker in all_workers if queue in worker.queues]
             queue_data['workers'] = len(queue_workers)
+
+            finished_job_registry = FinishedJobRegistry(queue.name, connection)
+            started_job_registry = StartedJobRegistry(queue.name, connection)
+            deferred_job_registry = DeferredJobRegistry(queue.name, connection)
+            queue_data['finished_jobs'] = len(finished_job_registry)
+            queue_data['started_jobs'] = len(started_job_registry)
+            queue_data['deferred_jobs'] = len(deferred_job_registry)
+
         queues.append(queue_data)
 
     context_data = {
